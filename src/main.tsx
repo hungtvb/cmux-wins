f92a26c7e1c9684169940e201e3c62f4425bcebe
hungtvb <69663872+hungtvb@users.noreply.github.com>
2026-07-25 23:21:42 -0700

    fix: load persistent workspace surface styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom/client";
 import "@xterm/xterm/css/xterm.css";
 import App from "./App";
 import "./styles.css";
+import "./workspace-surfaces.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
