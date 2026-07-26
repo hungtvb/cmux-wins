31463172a06d010be9f04cd08b091c5ca25bef9a
hungtvb <69663872+hungtvb@users.noreply.github.com>
2026-07-26 01:16:35 -0700

    style: load workspace controls

--- src/main.tsx
+++ src/main.tsx
@@ -4,5 +4,6 @@ import "@xterm/xterm/css/xterm.css";
 import App from "./App";
 import "./styles.css";
 import "./workspace-surfaces.css";
+import "./workspace-controls.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
