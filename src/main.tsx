0e538d7b8653dc147eca955b39d1a447a1e566f2
hungtvb <69663872+hungtvb@users.noreply.github.com>
2026-07-31 09:00:10 +0700

    style: load workspace metadata styles

--- src/main.tsx
+++ src/main.tsx
@@ -6,5 +6,6 @@ import "./styles.css";
 import "./workspace-surfaces.css";
 import "./workspace-controls.css";
 import "./browser-pane.css";
+import "./workspace-metadata.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
