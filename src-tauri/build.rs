3f9019330377fa077628da578d9acf22846fb8da
hungtvb <69663872+hungtvb@users.noreply.github.com>
2026-08-01 15:03:02 +0700

    chore: remove ineffective unit-test linker manifest flags

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,21 +1,3 @@
-use std::path::PathBuf;
-
 fn main() {
     tauri_build::build();
-
-    if std::env::var_os("CARGO_CFG_WINDOWS").is_some() {
-        let manifest = PathBuf::from(
-            std::env::var_os("CARGO_MANIFEST_DIR")
-                .expect("CARGO_MANIFEST_DIR must be set for build scripts"),
-        )
-        .join("windows")
-        .join("test.manifest");
-
-        println!("cargo:rerun-if-changed={}", manifest.display());
-        println!("cargo:rustc-link-arg-tests=/MANIFEST:EMBED");
-        println!(
-            "cargo:rustc-link-arg-tests=/MANIFESTINPUT:{}",
-            manifest.display()
-        );
-    }
 }
